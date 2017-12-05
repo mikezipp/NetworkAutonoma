4deae90978b5a86bd23afe916bfa5ba7376d0755
Mike Zipp <mzipp@ext.uber.com>
2017-12-04 17:33:23 -0800

    more updates

--- network-automation.py
+++ network-automation.py
@@ -1,8 +1,8 @@
-'''
+"""
 This module is a base for single-thread scripts.
 based on https://github.com/admiralspark/NetSpark-Scripts
 sudo python -m pip install netmiko <- TO INSTALL NETMIKO
-'''
+"""
 
 from datetime import datetime
 import csv
@@ -16,51 +16,113 @@ TOTALTIME = ENDTIME - STARTTIME
 
 COMMANDLIST = []
 TARGETDEVICES = []
+hostname = []
+device_type = []
+ipaddr = []
+username = []
+password = []
+secret = []
+device = {}
+
+
+
 
 # Iterates through a CSV, forms a dict, runs the command and logics it.
 
-def initlists():
-    with open("company.csv", mode='r') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            hostname = row['SysName']
-            device_type = row['device_type']
-            ipaddr = row['IP_Address']
-            username = row['username']
-            password = row['password']
-            secret = row['secret']
-            device = {
-                'device_type': device_type,
-                'ip': ipaddr,
-                'username': username,
-                'password': password,
-                'secret': secret,
-                'verbose': False,
-            }
+
+def userselect1():
+   with open("company.csv", mode='r') as csvfile:
+      reader = csv.DictReader(csvfile)
+#      rows = [r for r in reader]
+      count = 1
+
+      for count, row in enumerate(reader):
+         hostname = row['SysName']
+         device_type = row['device_type']
+         ipaddr = row['IP_Address']
+         username = row['username']
+         password = row['password']
+         secret = row['secret']
+         count = count + 1
+         device = {
+            'device_type': device_type,
+            'ip': ipaddr,
+            'username': username,
+            'password': password,
+            'secret': secret,
+            'verbose': False,
+      }
+         print count, device
+
+#ADD FUNCTION TO ALLOW USER TO APPLY CONFIG ONLY TO SPECIFIED DEVICE
 
 
-def main():
-   initlists()
-   COMMANDSTRING = raw_input("\nEnter command to run: \n> ")
-   if COMMANDSTRING != "":
-      COMMANDLIST.append(COMMANDSTRING)
-      print "you entered %s" % COMMANDLIST
-      COMMANDTARGET = raw_input("\nEnter target device ip: \n> ")
-      TARGETDEVICES.append(COMMANDTARGET)
-      print "you selected %s" % TARGETDEVICES
-      for device in TARGETDEVICES:
-         if device in ipaddr:
-            print "found device, starting command push on %s" % device
-#           netcon(username, password, COMPANY, COMMANDLIST
+
+
+def userselect2():
+   with open("company.csv", mode='r') as csvfile:
+      reader = csv.DictReader(csvfile)
+      for row in reader:
+         hostname = row['SysName']
+         device_type = row['device_type']
+         ipaddr = row['IP_Address']
+         username = row['username']
+         password = row['password']
+         secret = row['secret']
+         device = {
+            'device_type': device_type,
+            'ip': ipaddr,
+            'username': username,
+            'password': password,
+            'secret': secret,
+            'verbose': False,
+      }
+
+         COMMAND = raw_input ("enter command would you like to run\n>")
+         print "You entered \n\n%s\n\nIf this looks correct, enter YES. enter NO to abort" % COMMAND
+         COMMANDCONFIRM = raw_input ("\n>")
+         if COMMANDCONFIRM == "YES":
+            print "Sending config:\n\n%s\n\non %s with the following credentials:\nusername: %s\npassword: %s" % (COMMAND, ipaddr, username, password)
+#           net_connect = ConnectHandler(**device)
+#           showcommand = net_connect.send_command_expect("show system uptime")
+#           print("\n\n>>>>>>>>> Device {0} <<<<<<<<<\n".format(row['SysName']))
+#           configcommand = "do show system uptime "
+#           exitcommand = "exit"
             break
-         else:
-            print "could not find device %s" % device
+         elif COMMANDCONFIRM == "NO":
+            print "you selected NO. Exiting program"
             break
 
-main()
+#         if iphost == "":
+#            net_connect.config_mode()
+#            net_connect.send_command_expect(showcommand)
+#            print "Sent command %s to %s " % (showcommand, ipaddr)
+#            net_connect.send_command_expect(configcommand)
+#            print "Sent command %s to %s " % (configcommand, ipaddr)
+#            print "Memory Saved."
+#            print "\n>>>>>>>>> End <<<<<<<<<"
+#            net_connect.disconnect()
 
 
 
+
+
+def usermenu():
+   userselect = raw_input ("\n\nWhat would you like to do?\n1 - Run a single command against a single host\n2 - Run a single command against a list of hosts\n4 - Exit program\n\n>")
+
+   if userselect == "1":
+      print "you selected run a single command against a single host"
+      userselect1()
+
+   if userselect == "2":
+      print "you selected run a single command against a list of hosts"
+      userselect2()
+
+
+   if userselect == "4":
+      print "exiting program"
+
+usermenu()
 
 
 
